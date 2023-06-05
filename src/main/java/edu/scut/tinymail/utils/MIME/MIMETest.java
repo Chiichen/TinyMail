9ba11b8b902a1563ca679691f080710d0b3d5ce8
JC <2530221402@qq.com>
2023-06-05 21:56:43 +0800

    MIME DONE!!!!!!!!!!!!!!!

--- src/main/java/edu/scut/tinymail/utils/MIME/MIMETest.java
+++ src/main/java/edu/scut/tinymail/utils/MIME/MIMETest.java
@@ -1,12 +1,12 @@
 package edu.scut.tinymail.utils.MIME;
 
-import jakarta.activation.MimetypesFileTypeMap;
 import org.junit.Test;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
 
 
@@ -18,80 +18,113 @@ public class MIMETest {
     }
 
     public void MIME() throws IOException {
-        // 邮件服务器地址和端口
-        String smtpServer = "smtp.163.com";
-        int port = 587;
+        final String SMTP_SERVER = "smtp.qq.com";
+        final int SMTP_PORT = 25;
 
-        // 发件人、收件人、用户名和密码
-        String from = "m15017686102@163.com";
-        String to = "253221402@qq.com";
-        String user = "m15017686102@163.com";
-        String password = "KDONRCKHYQIJKRAF";
+        final String USERNAME = "2530221402@qq.com";
+        final String PASSWORD = "aaievxjsnpavebdi";
 
-        // 邮件主题和正文
-        String subject = "Subject of the Email";
-        String body = "This is the body of the email";
+        try {
+            // 创建Socket连接
+            Socket socket = new Socket(SMTP_SERVER, SMTP_PORT);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        // 附件文件名和路径
-        String attachmentFilename = "a.txt";
-        String attachmentPath = "D:\\IDEA\\IdeaProjects\\mail-demo\\a.txt";
+            // 读取服务器响应
+            String response = reader.readLine();
+            System.out.println(response);
 
-        // 连接到邮件服务器
-        Socket socket = new Socket(smtpServer, port);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            // 发送SMTP命令
+            sendCommand(writer, "HELO qq.com");
+            response = reader.readLine();
+            System.out.println(response);
 
-        // 读取服务器响应
-        String response = reader.readLine();
-        System.out.println(response);
+            // 发送身份验证命令
+            sendCommand(writer, "AUTH LOGIN");
+            response = reader.readLine();
+            System.out.println(response);
 
-        // 发送EHLO命令
-        sendCommand(writer, reader, "EHLO " + smtpServer);
+            // 发送用户名
+            sendCommand(writer, base64Encode(USERNAME));
+            response = reader.readLine();
+            System.out.println(response);
 
-        // 发送AUTH LOGIN命令并进行身份验证
-        sendCommand(writer, reader, "AUTH LOGIN");
-        sendCommand(writer, reader, Base64.getEncoder().encodeToString(user.getBytes()));
-        sendCommand(writer, reader, Base64.getEncoder().encodeToString(password.getBytes()));
+            // 发送密码
+            sendCommand(writer, base64Encode(PASSWORD));
+            response = reader.readLine();
+            System.out.println(response);
 
-        // 发送MAIL FROM命令
-        sendCommand(writer, reader, "MAIL FROM:<" + from + ">");
+            // 发送发件人信息
+            sendCommand(writer, "MAIL FROM: <2530221402@qq.com>");
+            response = reader.readLine();
+            System.out.println(response);
 
-        // 发送RCPT TO命令
-        sendCommand(writer, reader, "RCPT TO:<" + to + ">");
+            // 发送收件人信息
+            sendCommand(writer, "RCPT TO: <1405522135@qq.com>");
+            response = reader.readLine();
+            System.out.println(response);
 
-        // 发送DATA命令
-        sendCommand(writer, reader, "DATA");
+            // 开始邮件内容
+            sendCommand(writer, "DATA");
+            response = reader.readLine();
+            System.out.println(response);
 
-        // 发送邮件头部信息
-        writer.write("From: " + from + "\r\n");
-        writer.write("To: " + to + "\r\n");
-        writer.write("Subject: " + subject + "\r\n");
+            // 发送邮件头部
+            writer.println("From: 2530221402@qq.com");
+            writer.println("To:1277719855@qq.com");
+            writer.println("Subject: Test Email");
+            writer.println("MIME-Version: 1.0");
+            //writer.println("charset=\"UTF-8\"");
+            writer.println("Content-Transfer-Encoding: base64");
+            writer.println("Content-Type: multipart/mixed; boundary=boundary");
 
-        // 添加MIME版本和内容类型信息
-        writer.write("MIME-Version: 1.0\r\n");
-        writer.write("Content-Type: multipart/mixed; boundary=\"boundary\"\r\n\r\n");
+            // 发送邮件正文
+            writer.println("--boundary");
+            writer.println("Content-Type: text/plain");
+            writer.println();
+            writer.println("This is the body of the email.");
 
-        // 添加邮件正文
-        writer.write("--boundary\r\n");
-        writer.write("Content-Type: text/plain; charset=\"UTF-8\"\r\n\r\n");
-        writer.write(body + "\r\n\r\n");
+            // 发送附件
+            writer.println("--boundary");
+            writer.println("Content-Type: application/octet-stream");
+            writer.println("Content-Disposition: attachment; filename=\"file.txt\"");
+            writer.println();
 
-        // 添加附件
-        writer.write("--boundary\r\n");
-        writer.write("Content-Type: text/plain; name=\"" + attachmentFilename + "\"\r\n");
-        writer.write("Content-Disposition: attachment; filename=\"" + attachmentFilename + "\"\r\n\r\n");
+            // 读取附件内容并进行Base64编码后发送
+            Path attachmentPath = Paths.get("D:\\IDEA\\IdeaProjects\\mail-demo\\mail-demo\\src\\com\\cc4cin\\mailtest\\file.txt");
+            byte[] attachmentBytes = Files.readAllBytes(attachmentPath);
+            String base64EncodedAttachment = Base64.getEncoder().encodeToString(attachmentBytes);
+            writer.println(base64EncodedAttachment);
+
+            // 结束邮件内容
+            writer.println();
+            writer.println("--boundary--");
+            writer.println(".");
+            writer.flush();
+
+            response = reader.readLine();
+            System.out.println(response);
+
+            // 结束会话
+            sendCommand(writer, "QUIT");
+            response = reader.readLine();
+            System.out.println(response);
+
+            // 关闭连接
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void sendCommand(BufferedWriter writer, BufferedReader reader, String command) throws IOException {
-        // 向服务器发送命令
-        writer.write(command + "\r\n");
+    private static void sendCommand(PrintWriter writer, String command) {
+        writer.println(command);
         writer.flush();
-
-        // 读取服务器响应
-        String response = reader.readLine();
-        System.out.println(response);
     }
 
+    private static String base64Encode(String text) {
+        return Base64.getEncoder().encodeToString(text.getBytes());
+    }
 
     public String getMimeType(String filePath) throws IOException {
         Path path = new File(filePath).toPath();
