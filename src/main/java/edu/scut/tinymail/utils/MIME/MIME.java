9edd19ee3175328598bc7125ee68e0cfa70968ff
JC <2530221402@qq.com>
2023-06-14 20:20:35 +0800

    将MIME类中的sender方法改写为多个分开的可连续调用的方法

--- src/main/java/edu/scut/tinymail/utils/MIME/MIME.java
+++ src/main/java/edu/scut/tinymail/utils/MIME/MIME.java
@@ -12,11 +12,15 @@ import java.util.Base64;
 
 @Data
 public class MIME {
+
     @Test
     public void test() throws IOException {
         MIMESENDER();
     }
 
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private Socket socket;
     //SMTP服务器
     String SMTP_SERVER = "smtp.qq.com";
     //服务器域名
@@ -42,6 +46,112 @@ public class MIME {
     //MIME类型
     String MIME_TYPE = "";
 
+    //MIME初始化接口
+    public MIME Initialize(String server, int port, String domain, String username, String psw) throws IOException {
+        //参数初始化
+        setSMTP_SERVER(server);
+        setSMTP_PORT(port);
+        setDOMAIN_NAME(domain);
+        setUSERNAME(username);
+        setPASSWORD(psw);
+
+        // 创建Socket连接
+        setSocket(new Socket(this.SMTP_SERVER, this.SMTP_PORT));
+        setReader(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+        setWriter(new PrintWriter(new OutputStreamWriter(socket.getOutputStream())));
+
+        // 读取服务器响应
+        System.out.println(reader.readLine());
+
+        // 发送SMTP命令
+        sendCommand(writer, "HELO " + DOMAIN_NAME);
+        System.out.println(reader.readLine());
+
+        // 发送身份验证命令
+        sendCommand(writer, "AUTH LOGIN");
+        System.out.println(reader.readLine());
+
+        // 发送用户名
+        sendCommand(writer, base64Encode(this.USERNAME));
+        System.out.println(reader.readLine());
+
+        // 发送密码
+        sendCommand(writer, base64Encode(this.PASSWORD));
+        System.out.println(reader.readLine());
+        return this;
+    }
+
+    public MIME sendInfo(String sender, String receiver) throws IOException {
+        // 发送发件人信息
+        sendCommand(writer, "MAIL FROM: <" + sender + ">");
+        System.out.println(reader.readLine());
+
+        // 发送收件人信息
+        sendCommand(writer, "RCPT TO: <" + receiver + ">");
+        System.out.println(reader.readLine());
+        return this;
+    }
+
+    public MIME sendDataStart(String sender, String receiver, String subject) throws IOException {
+        // 开始邮件内容
+        sendCommand(writer, "DATA");
+        System.out.println(reader.readLine());
+
+        // 发送邮件头部
+        sendCommand(writer,"From: " + sender);
+        sendCommand(writer,"To: " + receiver);
+        sendCommand(writer,"Subject: " + subject);
+        sendCommand(writer,"MIME-Version: 1.0");
+        sendCommand(writer,"Content-Transfer-Encoding: base64");
+        sendCommand(writer,"Content-Type: multipart/mixed; boundary=boundary");
+        sendCommand(writer,"");
+
+       return this;
+    }
+
+    // 发送邮件正文
+    public MIME sendContent(String content){
+        sendCommand(writer,"--boundary");
+        sendCommand(writer,"Content-Type: text/plain");
+        sendCommand(writer,"");
+        sendCommand(writer, content);
+        return this;
+    }
+
+    public MIME sendAttachment(String filename, String filepath) throws IOException {
+        // 发送附件
+        sendCommand(writer, "--boundary");
+        sendCommand(writer,"Content-Type: application/octet-stream");
+        sendCommand(writer,"Content-Transfer-Encoding: base64");
+        sendCommand(writer,"Content-Disposition: attachment;filename=\"" + filename + "\"");
+        writer.println();
+
+        // 读取附件内容并进行Base64编码后发送
+        Path attachmentPath = Paths.get(filepath);
+        byte[] attachmentBytes = Files.readAllBytes(attachmentPath);
+        String base64EncodedAttachment = Base64.getEncoder().encodeToString(attachmentBytes);
+        sendCommand(writer, base64EncodedAttachment);
+
+        return this;
+    }
+
+    public MIME sendDataEnd() throws IOException {
+        // 结束邮件内容
+        writer.println();
+        writer.println("--boundary--");
+        writer.println(".");
+        writer.flush();
+        System.out.println(reader.readLine());
+
+        // 结束会话
+        sendCommand(writer, "QUIT");
+        System.out.println(reader.readLine());
+
+        // 关闭连接
+        socket.close();
+
+        return this;
+    }
 
     public void MIMESENDER() {
         try {
