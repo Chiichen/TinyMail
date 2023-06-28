fc77803d21d661f80c9cb4f3fb2298f2f5fe5c5a
scutfreshman <1120058125@qq.com>
2023-06-28 22:39:18 +0800

    进行Set页面的一定补充，完善，等待华子的数据同步添加

--- src/components/Set.vue
+++ src/components/Set.vue
@@ -1,18 +1,85 @@
 <template>
     <div class="setBody">
-        <div class="setMain">
+
+        <div class="setQQmail" @click="SelectQQmail">
+            <img src="/img/qqmail_logo.png" alt="QQLogo" class="setQQLogo">
+
+        </div>
+
+        <div class="set163mail" @click="Select163mail">
+            <img src="/img/163mail_logo.png" alt="163Logo" class="set163Logo">
+        </div>
+
+        <div class="setGmail" @click="SelectGmail">
+            <img src="/img/gmail_logo.png" alt="GoogleLogo" class="setGoogleLogo">
+            <span>Gmail</span>
+        </div>
+
+        <!-- 设置选择QQ邮箱添加或者默认时的页面 -->
+        <div class="setQQMain" v-if="select==1">
             <div class="setHeader">
                 <el-button class="cancelButton" text style="font-size: large;" @click="return_back"><b>取消</b></el-button>
                 <el-button text style="font-size: large;" @click="want_help"><b>帮助</b></el-button>
             </div>
-            <div class="setLogo">
-                <img src="img/logo.png" alt="Logo" >
+
+            <div class="showQQLogo" >
+                <img src="img/qqmail_logo.png" alt="Logo" >
             </div>
+            
             <div class="setContent">
-                    <input :model="addEmail" placeholder="输入邮箱地址" type="text" style="width:250px" size="large" class="inputForm"/>
+                    <input :model="addQQmail" placeholder="输入QQ邮箱地址" type="text" style="width:250px" size="large" class="inputForm"/>
                 
                 <el-form-item>
-                    <el-button type="primary" size="large" class="addButton" @click="add()"><el-icon><Pointer /></el-icon>添加账号</el-button>
+                    <el-button type="primary" size="large" class="addButton" @click="add_QQmail()"><el-icon><Pointer /></el-icon>添加邮箱</el-button>
+                </el-form-item>
+
+                <el-form-item>
+                    <img src="img/set-img.png" alt="Logo" class="setImg">
+                </el-form-item>
+                
+            </div>
+        </div>
+
+        <!-- 设置选择163邮箱时的页面 -->
+        <div class="set163Main" v-if="select==2">
+            <div class="setHeader">
+                <el-button class="cancelButton" text style="font-size: large;" @click="return_back"><b>取消</b></el-button>
+                <el-button text style="font-size: large;" @click="want_help"><b>帮助</b></el-button>
+            </div>
+
+            <div class="show163Logo" >
+                <img src="img/163mail_logo.png" alt="Logo" style="height:30px;width: 200px;">
+            </div>
+            
+            <div class="setContent">
+                    <input :model="add163mail" placeholder="输入163邮箱地址" type="text" style="width:250px" size="large" class="inputForm"/>
+                
+                <el-form-item>
+                    <el-button type="primary" size="large" class="addButton" @click="add_163mail()"><el-icon><Pointer /></el-icon>添加邮箱</el-button>
+                </el-form-item>
+
+                <el-form-item>
+                    <img src="img/set-img.png" alt="Logo" class="setImg">
+                </el-form-item>
+                
+            </div>
+        </div>
+
+        <!-- 设置选择谷歌邮箱的页面 -->
+        <div class="setGMain" v-if="select==3">
+            <div class="setHeader">
+                <el-button class="cancelButton" text style="font-size: large;" @click="return_back"><b>取消</b></el-button>
+                <el-button text style="font-size: large;" @click="want_help"><b>帮助</b></el-button>
+            </div>
+            
+            <div class="showGLogo" >
+                <img src="img/gmail_logo.png" alt="Logo" style="height:75px;padding-left: 18%;">
+            </div>
+            <div class="setContent">
+                    <input :model="addGmail" placeholder="输入谷歌邮箱地址" type="text" style="width:250px" size="large" class="inputForm"/>
+                
+                <el-form-item>
+                    <el-button type="primary" size="large" class="addButton" @click="add_Gmail()"><el-icon><Pointer /></el-icon>添加邮箱</el-button>
                 </el-form-item>
 
                 <el-form-item>
@@ -29,11 +96,24 @@
 export default{
     data () {
         return {
-            addEmail:'',
+            addQQmail:'',
+            add163mail:'',
+            addGmail:'',
+            select:1,
         }
     },
     methods: {
-        add(){
+        /* 记录用户输入的不同类型邮箱 */
+        add_QQmail(){
+            
+            this.$router.push("/home");
+        },
+        add_163mail(){
+            
+            this.$router.push("/home");
+        },
+        add_Gmail(){
+            
             this.$router.push("/home");
         },
         return_back(){
@@ -42,6 +122,15 @@ export default{
         want_help(){
 
         },  
+        SelectQQmail(){
+            this.select=1;
+        },
+        Select163mail(){
+            this.select=2;
+        },
+        SelectGmail(){
+            this.select=3;
+        },
     },
 }
 </script>
@@ -54,16 +143,110 @@ export default{
   height: 100%;
 }
 
-.setMain{
+.setQQmail{
+    position: absolute;
+    top: 40%;
+  left: 40%;
+  border:8px solid rgb(76, 81, 237);
+  border-radius: 50%;
+  background-color:rgba(226, 232, 248, 0.75);
+  margin-top: -200px;
+  margin-left: -250px;
+  width: 260px;
+  height: 150px;
+}
+.setQQLogo{
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform:translate(-50%,-50%);
+}
+
+.set163mail{
+    position: absolute;
+    top: 60%;
+  left: 20%;
+  border:8px solid rgb(76, 81, 237);
+  border-radius: 50%;
+  background-color:rgba(226, 232, 248, 0.75);
+  margin-top: -200px;
+  margin-left: -250px;
+  width: 260px;
+  height: 150px;
+}
+.set163Logo{
+    position: absolute;
+    top:50%;
+    left:50%;
+    height: 25%;
+    transform:translate(-50%,-50%);
+}
+.setGmail{
+    position: absolute;
+    top: 80%;
+  left: 40%;
+  border:8px solid rgb(76, 81, 237);
+  border-radius: 50%;
+  background-color:rgba(226, 232, 248, 0.75);
+  margin-top: -200px;
+  margin-left: -250px;
+  width: 260px;
+  height: 150px;
+}
+.setGoogleLogo{
+    position: absolute;
+    top:35%;
+    left:50%;
+    height: 35%;
+    transform:translate(-50%,-50%);
+}
+.setGmail span{
+    position: absolute;
+    top: 60%;
+    left:50%;
+    transform: translate(-50%,-50%);
+    font-size:38px;
+    font-weight: bold;
+    letter-spacing: 3px;
+    font-family: "DejaVu Sans Mono";
+    color: rgb(208, 94, 94);
+}
+
+.setQQMain{
     position: absolute;
   top: 40%;
-  left: 55%;
+  left: 70%;
   margin-top: -200px;
   margin-left: -250px;
   width: 350px;
   height: 500px;
   background: rgb(255, 255, 255);
   border-radius: 5%;
+  opacity: 90%;
+}
+.set163Main{
+    position: absolute;
+  top: 40%;
+  left: 70%;
+  margin-top: -200px;
+  margin-left: -250px;
+  width: 350px;
+  height: 500px;
+  background: rgb(255, 255, 255);
+  border-radius: 5%;
+  opacity: 90%;
+}
+.setGMain{
+    position: absolute;
+  top: 40%;
+  left: 70%;
+  margin-top: -200px;
+  margin-left: -250px;
+  width: 350px;
+  height: 500px;
+  background: rgb(255, 255, 255);
+  border-radius: 5%;
+  opacity: 90%;
 }
 
 .setHeader{
@@ -78,13 +261,23 @@ export default{
     margin-right: 180px;
 }
 
-.setLogo{
-    padding-top:5%;
-    padding-left: 30px;
+.showQQLogo{
+    padding-top:10%;
+    padding-left: 80px;
     height: 10px;
 
 }
-
+.show163Logo{
+    padding-top:10%;
+    padding-left: 80px;
+    height: 10px;
+   
+}
+.showGLogo{
+    padding-top:10%;
+    padding-left: 80px;
+    height: 10px;
+}
 
 .setContent{
     padding-top:100px;
